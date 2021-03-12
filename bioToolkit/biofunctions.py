c2b81f6a675f57aef635cf4cc0fa26a55e0b709f
RodrigoMilan <rodrigomcalegari@gmail.com>
2021-03-12 20:03:30 -0300

    adding translation function in biofunctions

--- bioToolkit/biofunctions.py
+++ bioToolkit/biofunctions.py
@@ -11,8 +11,33 @@ def transcription(dna):
     
     return tdna
 
-def tranlastion(dna):
-    pass
+def translastion(dna):
+  protein = ''
+  codon_table = {
+      'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+      'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+      'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+      'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+      'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+      'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+      'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+      'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+      'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+      'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+      'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+      'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+      'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+      'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+      'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+      'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+    for i in range(0,len(dna),3):
+    if len(dna[i:i+3]) == 3:
+      codon = codon_table[dna[i:i+3]]
+      if codon == '_':
+        break
+      protein += codon
+
+  return protein
 
 def reverseComplement(dna):
     conversion_table = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
