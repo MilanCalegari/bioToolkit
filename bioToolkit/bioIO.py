968f6e45b830698b626750f3047bd3a7c1bb79ad
RodrigoMilan <rodrigomcalegari@gmail.com>
2021-03-12 20:04:28 -0300

    adding FASTQ options at bioIO

--- bioToolkit/bioIO.py
+++ bioToolkit/bioIO.py
@@ -1,13 +1,33 @@
-class Fasta:
+
+# This class is called by fasta function Usage: x = fasta(fastqID, fastqSeq, fastqQuality)
+class Fasta: 
     def __init__(self, fastaId, fastaSeq):
         self.fastaId = fastaId 
         self.fastaSeq = fastaSeq
 
-    def sequences(self):
+    def sequences(self): #return the sequences from fasta file in a list (usage: x.sequences())
         return self.fastaSeq
-    def id(self):
+    def id(self): #return the id from fasta file in a list (usage: x.id())
         return self.fastaId
+    def write(self, outputName): #write a fasta file (usage: x.write("output.fasta"))
+        w = open(outputName, 'w')
+        for i in range(len(self.fastaId)):
+            w.write(f">{self.fastaId[i]}\n{self.fastaSeq[i]}\n")
+        w.close()
 
+# This class is called by fastq function  Usage: x = fastq(fastqID, fastqSeq, fastqQuality)
+class FastQ:
+    def __init__(self, fastqID, fastqSeq, fastqQuality):
+        self.fastqID = fastqID
+        self.fastqSeq = fastqSeq
+        self.fastqQuality = fastqQuality
+
+    def sequences(self): #return the sequences from fastq file in a list (usage: x.sequences())
+        return self.fastqSeq
+    def id(self): #return the id from fastq file in a list (usage: x.id())
+        return self.fastqID
+    def write(self, outputName): #write a fastq file (usage: x.write("output.fasta"))
+        return self.fastqQuality
 
 class BioReader:
     def __init__(self, file):
@@ -28,12 +48,27 @@ class BioReader:
         opened_fasta = fasta(fastaID, fastaSeq)
 
         return opened_fasta
-    
 
+    def readFastQ(self):
+        fastq_file = open(self.file, 'r')
+        fastq_file_line = fastq_file.readlines()
+        fastq_file.close()
 
+        fastqID, fastqSeq, fastqQuality = [], [], []
+
+        for l in range(len(fastq_file_line)-3):
+            if '@' in fastq_file_line[l]:
+                fastqID.append(fastq_file_line[l].rstrip().replace('@',''))
+                fastqSeq.append(fastq_file_line[l+1].rstrip())
+                fastqQuality.append(fastq_file_line[l+3].rstrip())
+
+        return fastqID, fastqSeq, fastqQuality
 
 def bioReader(file):
     return BioReader(file)
 
-def fasta(fastaId, fastaSeq):
-    return Fasta(fastaId, fastaSeq)
+def fasta(fastaID, fastaSeq):
+    return Fasta(fastaID, fastaSeq)
+
+def fastq(fastqID, fastqSeq, fastqQuality):
+    return FastQ(fastqID, fastqSeq, fastqQuality)
